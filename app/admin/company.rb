615d3c854ed87d2b64d01ea5611c4064450b5452
Prelang Builder <builder@prelang.com>
2015-06-25 00:34:27 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Company' to allow for create, update

--- app/admin/company.rb
+++ app/admin/company.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Company do
+  permit_params :firm_id, :city, :state, :symbol, :name, :description
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
