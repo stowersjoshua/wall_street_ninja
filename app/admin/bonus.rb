d7185998fb22a6a3d14f717138b73e83a040fc80
Prelang Builder <builder@prelang.com>
2015-06-25 00:34:43 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Bonus' to allow for create, update

--- app/admin/bonus.rb
+++ app/admin/bonus.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Bonus do
+  permit_params :amount, :description
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
