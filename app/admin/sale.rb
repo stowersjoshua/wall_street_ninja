a401258abc4003d8d33f07f58ad15cc41365843a
Prelang Builder <builder@prelang.com>
2015-06-25 00:34:36 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Sale' to allow for create, update

--- app/admin/sale.rb
+++ app/admin/sale.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Sale do
+  permit_params :firm_id, :type, :quantity, :value
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
