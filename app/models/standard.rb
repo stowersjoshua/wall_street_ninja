1b49ba106b45a03d39d720fc0e725a5dd3584e08
Abhinav <abhinav.odesk@ongraph.com>
2016-02-12 10:15:20 +0530

    student earning, purchase and sales

--- app/models/standard.rb
+++ app/models/standard.rb
@@ -1,2 +1,15 @@
 class Standard < User # :nodoc:
+
+	def total_purchase
+		self.active_portfolio.purchases.map(&:quantity).sum + self.total_sales
+	end
+
+	def total_sales
+		self.active_portfolio.sales.map(&:quantity).sum
+	end
+
+	def total_earning
+		earning = self.total_balance - 100000
+		return 0 if earning < 0
+	end
 end
